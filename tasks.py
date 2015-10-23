488a57ad3d94a057808f656bc2d3dd56d8848a0d
Karthik T <karthikt.holmes+github@gmail.com>
2015-10-24 00:25:41 +0800

    Initial Commit, extracted code from another project

---
+++ tasks.py
@@ -0,0 +1,31 @@
+from invoke import run, task
+from invoke.exceptions import Failure
+
+@task
+def test():
+	run("python -m unittest discover test")
+
+@task
+def freeze():
+	run("pip freeze > requirements.txt")
+
+@task(post=[freeze])
+def install(pkg, upgrade=False):
+	upgrade = "--upgrade" if upgrade else ""
+	run("pip install " + pkg + " " + upgrade, pty=True)
+
+@task(post=[freeze])
+def uninstall(pkg):
+	run("pip uninstall " + pkg, pty=True)
+
+@task
+def setup():
+	run("pip install -r requirements.txt", pty=True)
+
+@task
+def rerun():
+	 try:
+	 	return run("rerun -xp '**/*.py' invoke test").exited
+	 except Failure as e:
+	 	print "The tool rerun isnt installed, installing it now, please try again."
+	 	run("gem install rerun --no-ri --no-rdoc")
